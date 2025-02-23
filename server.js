efe700e0756cebc68086d83787cad3350cda7ae1
symonmuchemi <simonkenya846@gmail.com>
2025-02-23 20:02:52 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: 'Welcome DevOps!',
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
